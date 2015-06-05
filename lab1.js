db71db498ad1f0e0d0a7d489e5af7ad2f7b46e5b
macbook air <macbookair@macbooks-MacBook-Air.local>
2015-06-04 21:54:51 -0700

    first commit

--- lab1.js
+++ lab1.js
@@ -53,8 +53,17 @@ assert(1 === 2, 'this is an assertion failure example. 1 === 2');
  zoo-themed.  Make one pass and one fail. In the failure message, describe why
  it failed.
 */
+function assert(expression, failureMessage) {
+  if (!expression) {
+    console.log("assertion failure: ", failureMessage);
+  }
+}
 
 //your code goes here
+assert("cat"==="dog","cat is not stricty equal to dog");
+assert("penguin"==="penguin"," penguins may only live in south pole");
+// cat is not equal to dogs because the variable location in memory determine the value of each string. cat does not have the same value as dog.
+											//DONE
 
 /* ----------------- Meerkats -------------------------------------------------
  Meerkats make a sort of chirping noise (according to my 30 seconds of
@@ -64,6 +73,7 @@ assert(1 === 2, 'this is an assertion failure example. 1 === 2');
 var sentence1 = 'More food please.';
 var sentence2 = 'Come over here so you can scratch my belly.';
 
+
 /*
  TODO: 20 points
  Your goal is to replace the words in the above sentences with 'chirp' The
@@ -71,8 +81,31 @@ var sentence2 = 'Come over here so you can scratch my belly.';
  **two** different kinds of loops to implement this. (10 points each.)
  HINT: the 'split' method on String will be useful.
 */
+/*function assert(sentence,split) {
+  	var res=sentence.split("")
+  }
+}*/
+	var sentence1 = 'More food please.';
+var res=sentence1.split(" ");
+sentence1="";
+for(var i=1;i<res.length;i++)
+{
+	sentence1= sentence1+" chirp "
+
+}
+sentence.trim();
+sentence1= sentence1+" chirp. "
 
 //your code goes here
+var sentence2 = 'Come over here so you can scratch my belly.';
+var reso=sentence2.split(" ");
+sentence2=" ";
+for(var i=1;i<reso.length;i++)
+{
+	sentence2=sentence2+ " chirp ";
+}
+sentence2.trim();
+sentence2=sentence2+" chirp."
 
 assert(sentence1 === 'chirp chirp chirp.', 'sentence 1 should have 3 chirps');
 assert(sentence2 === 'chirp chirp chirp chirp chirp chirp chirp chirp chirp.',
@@ -84,7 +117,6 @@ assert(sentence2 === 'chirp chirp chirp chirp chirp chirp chirp chirp chirp.',
  to see next.
  Hint: read the Math.random description on MDN.
 */
-
 var favoriteAnimals = ['elephant', 'penguin', 'eagle', 'camel'];
 var nextAnimal;
 
@@ -92,8 +124,13 @@ var nextAnimal;
 // Assign one of your favorite animals to nextAnimal using Math.random() to pick
 
 // your code goes here
+var randomInd;
+var newAnimal;
+randomInd=Math.floor((Math.random() * favoriteAnimals.length));
+newAnimal=favoriteAnimals[randomInd];
+console.log(newAnimal);
 
-assert(nextAnimal, 'assign something to nextAnimal');
+assert(newAnimal, 'monkey');
 
 /* ----------------- Hungry Lion ----------------------------------------
  As long as the lion is well-fed, he doesn't take too much heed of the
@@ -119,6 +156,17 @@ var tooHungryDay;
 */
 
 // your code goes here
+var mealsPerDay = [5, 4, 3, 6, 2, 4, 3, 4, 5, 1];
+var tooHungryDay;
+var totalSum=0;
+var days=1;
+for(var i=0;i<mealsPerDay.length;i++)
+{
+	totalSum+=mealsPerDay[i];
+	tooHungryDay=totalSum/days++;
+	console.log(tooHungryDay);
+}
+
 
 assert(tooHungryDay, 'remember to assign the answer to tooHungryDay');
 assert(tooHungryDay < 10, 'the lion is too hungry before the end of the array');
